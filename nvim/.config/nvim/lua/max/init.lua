ac396f44b72a795c89c3b601870736d12376ac57
Maxed301 <66567365+Maxed301@users.noreply.github.com>
2023-11-29 12:50:04 +0100

    neovim & tmux config

---
+++ nvim/.config/nvim/lua/max/init.lua
@@ -0,0 +1,6 @@
+require("max.remap")
+require("max.set")
+-- require("hardtime").setup()
+require("rose-pine").setup({
+    disable_italics = true,
+})
---
+++ nvim/.config/nvim/lua/max/packer.lua
@@ -0,0 +1,66 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+
+    vim.cmd('colorscheme rose-pine')
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
+    use { 'nvim-treesitter/playground' }
+    use { 'theprimeagen/harpoon' }
+    use { 'mbbill/undotree' }
+    use { 'tpope/vim-fugitive' }
+    use { 'christoomey/vim-tmux-navigator' }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    use { 'ThePrimeagen/vim-be-good' }
+    use { 'github/copilot.vim' }
+    use { 'm4xshen/hardtime.nvim' }
+    use { 'simrat39/rust-tools.nvim' }
+end)
