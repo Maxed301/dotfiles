b1eef627ae7cb24c55130ad526b59aa56ad637f9
Maxed301 <66567365+Maxed301@users.noreply.github.com>
2025-04-27 14:37:23 +0200

    lualine lsp symbols

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -12,23 +12,33 @@ return {
 			end
 		end
 
-		local lsp_component = {
+		local lsp_client = {
 			function()
-				local msg = "No Active Lsp"
+				local msg = "No Active LSP"
 				local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
 				local clients = vim.lsp.get_clients()
 				if next(clients) == nil then
 					return msg
 				end
 				for _, client in ipairs(clients) do
-					local filetypes = client.config.filetypes
-					if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-						return " LSP: " .. client.name
+					local ft = client.config.filetypes
+					if ft and vim.fn.index(ft, buf_ft) ~= -1 then
+						return " " .. client.name
 					end
 				end
 				return msg
 			end,
 			color = { fg = colors.magenta2, gui = "bold" },
+		}
+
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_lsp" },
+			sections = { "error", "warn", "info", "hint" },
+			symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			colored = true,
+			update_in_insert = false,
+			always_visible = false,
 		}
 
 		require("lualine").setup({
@@ -39,8 +49,8 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch" },
-				lualine_c = { "filename" },
-				lualine_x = { lsp_component, "encoding", "fileformat", "filetype" },
+				lualine_c = { "filename", diagnostics },
+				lualine_x = { lsp_client, "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
